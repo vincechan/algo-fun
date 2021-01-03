6110394484786a6faedd75987ba781765dea3492
Vince Chan <vince.yp.chan@gmail.com>
2021-01-03 14:52:21 -0800

    connect level order siblings

--- src/patterns/tree-breadth-first-search/binary-tree-node.ts
+++ src/patterns/tree-breadth-first-search/binary-tree-node.ts
@@ -4,9 +4,28 @@
 export class BinaryTreeNode<T> {
   left?: BinaryTreeNode<T>;
   right?: BinaryTreeNode<T>;
+  next?: BinaryTreeNode<T>;
   value: T;
 
   constructor(value: T) {
     this.value = value;
   }
 }
+
+/**
+ * Export all node values of a Binary Tree to an array, traversing the tree using the "next" pointer.
+ *
+ * @param root
+ */
+export const export_values_by_successor = function <T>(
+  root: BinaryTreeNode<T>
+): T[] {
+  const result = [] as T[];
+  let node = root as BinaryTreeNode<T> | null;
+  while (node) {
+    result.push(node.value);
+    node = node.next || null;
+  }
+
+  return result;
+};
---
+++ src/patterns/tree-breadth-first-search/connect-level-order-siblings.ts
@@ -0,0 +1,38 @@
+import { BinaryTreeNode } from "./binary-tree-node";
+
+/**
+ * Given a binary tree, connect each node with its level order successor. The last node of each level should point to a null node.
+ *
+ * @param {BinaryTreeNode<T>} root - root node of a binary tree. The tree nodes "next" pointers have not been connected to its successor.
+ */
+export const connect_level_order_siblings = function <T>(
+  root: BinaryTreeNode<T>
+) {
+  const queue = [] as BinaryTreeNode<T>[];
+  let prev_node = null as BinaryTreeNode<T> | null;
+
+  if (root) {
+    queue.push(root);
+  }
+
+  while (queue.length) {
+    // remove node
+    const node = queue.shift() as BinaryTreeNode<T>;
+
+    // connect successor
+    if (prev_node) {
+      prev_node.next = node;
+    }
+
+    // add child nodes
+    if (node.left) {
+      queue.push(node.left);
+    }
+    if (node.right) {
+      queue.push(node.right);
+    }
+
+    // update prev_node
+    prev_node = node;
+  }
+};
