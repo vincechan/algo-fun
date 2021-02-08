51b6a24c1adf5ce125ef600bc60772975727cd31
Vince Chan <vince.yp.chan@gmail.com>
2021-02-07 22:49:41 -0800

    minimum window sort with O(n) method

--- src/patterns/two-pointers/minimum-window-sort.ts
+++ src/patterns/two-pointers/minimum-window-sort.ts
@@ -6,11 +6,48 @@ import { readFile } from "fs";
  * @param arr
  */
 export function shortest_window_sort(arr: number[]): number {
-  return use_sorted_array(arr);
+  // return use_sorted_array(arr);
+
+  // 1. find first unsorted element from the beginning
+  let left = 0;
+  while (left < arr.length - 1 && arr[left] <= arr[left + 1]) {
+    left++;
+  }
+
+  if (left == arr.length - 1) {
+    // all elements sorted
+    return 0;
+  }
+
+  // 2. find first unsorted element from the end
+  let right = arr.length - 1;
+  while (right > 0 && arr[right - 1] <= arr[right]) {
+    right--;
+  }
+
+  // 3. find min and max in the unsorted subarray
+  let min = Infinity,
+    max = -Infinity;
+  for (let i = left; i <= right; i++) {
+    min = Math.min(arr[i], min);
+    max = Math.max(arr[i], max);
+  }
+
+  // 4. extend subarray to include any number bigger than min on the left side of subarray
+  while (left >= 0 && arr[left - 1] > min) {
+    left--;
+  }
+
+  // 5. extend subarray to include any number smaller than max on the right side of subarray
+  while (right <= arr.length - 1 && arr[right + 1] < max) {
+    right++;
+  }
+
+  return right - left + 1;
 }
 
 function use_sorted_array(arr: number[]): number {
-  // this is not optimial -> uses sorted array and need nlogn time
+  // this is not optimial -> use sorted array, need nlogn time
 
   // create a sorted array
   // find start of the unsorted subarray
